1d4242136a0b6da990065521da1501076654e86e
Ati400 <assefaa@augsburg.edu>
2026-09-22 12:11:29 -0500

    Complete requirement 1 password length

--- src/PasswordValidator.java
+++ src/PasswordValidator.java
@@ -7,6 +7,13 @@
  *
  * @author 17133
  */
+
 public class PasswordValidator {
-    
+
+    public boolean isValid(String password) {
+        if (password.length() >= 8) {
+            return true;
+        }
+        return false;
+    }
 }
